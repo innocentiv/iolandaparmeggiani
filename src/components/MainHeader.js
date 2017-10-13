12d4bd0b944fe64cd4728859c05def2b4ec2d36e
Valerio Innocenti Sedili <valerio.innocenti.ext@yoox.com>
2017-10-13 23:34:21 +0200

    home page css

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -2,13 +2,15 @@ import React from 'react';
 import GatsbyLink from 'gatsby-link';
 
 import styles from '../css/main.module.css';
+import logo from '../images/logo.png';
 
 const MainHeader = () => (
   <header className={styles.header}>
     <GatsbyLink to="/" className={styles.link}>
+      <img src={logo} className={styles.logo} alt="Logo" />
       <h1 className={styles.title}>Iolanda Parmeggiani</h1>
     </GatsbyLink>
-    <ul>
+    <ul className={styles.navigation}>
         <li><GatsbyLink to="/illustration/">Illustration</GatsbyLink></li>
         <li><GatsbyLink to="/graphic/">Graphic</GatsbyLink></li>
         <li><GatsbyLink to="/video/">Video</GatsbyLink></li>
